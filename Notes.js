9a96b45ccbe38407819619f2d48329408d9fb297
Abhishek <sonukr24082001@gmail.com>
2024-07-03 16:50:46 +0530

    not

---
+++ Notes.js
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class Notes extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Notes</Text>
+      </View>
+    )
+  }
+}
+
+export default Notes
