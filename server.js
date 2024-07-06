c033342a266da716f58615614f7d13aed23d2df8
Abhishek <sonukr24082001@gmail.com>
2024-07-06 16:22:22 +0530

    vercel

--- server.js
+++ server.js
@@ -23,7 +23,11 @@ app.use(express.json());
 app.use("/api/v1/capture", captureRoutes);
 
 
-const PORT = 8000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.get("/", (req, res) => {
+  res.status(200).send("API is running");
 });
+
+// const PORT = 8000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
