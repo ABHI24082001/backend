00cbe4255ce62ac4e04592ae7870aff79ba25101
Abhishek <sonukr24082001@gmail.com>
2024-07-06 17:09:48 +0530

    base uel fix

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
-import express  from "express";
-import  dbConnect  from "./config/dbconnect.js";
+// index.js
+
+import express from "express";
+import dbConnect from "./config/dbconnect.js";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 
 import captureRoutes from "./routes/captureRoutes.js";
 
-
-
-
 const app = express();
-dbConnect()
+
+dbConnect();
 dotenv.config();
 
 app.use(morgan("dev"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-
+const PORT = 8000;
+const BASE_URL = process.env.BASE_URL || `http://localhost:${PORT}`;
 
 app.use("/api/v1/capture", captureRoutes);
 
-
-const PORT = 8000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log(`Base URL: ${BASE_URL}`);
 });
